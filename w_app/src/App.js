50d0e17531d4016a5ba44c1d79f5321b116bf648
yurlov-ra <Vldzin212523>
2019-07-26 18:34:30 +0500

    ...

--- w_app/src/App.js
+++ w_app/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
         <div className="spinner-wrap">
           <section className="spinner">
             <Spinner animation="grow" variant="primary" />{" "}
-            <span className="spinner-text">Идет загрузка услуг...</span>
+            <span className="spinner-text">Идет загрузка...</span>
           </section>
         </div>
       );
--- w_app/src/Components/Footer/Footer.js
+++ w_app/src/Components/Footer/Footer.js
@@ -4,7 +4,12 @@ import "./footer.scss";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>Наш адрес: г. Пермь 	<span className="footer-smile">&#128523;</span></p>
+      <p>
+        Наш адрес: г. Пермь{" "}
+        <span className="footer-smile" role="img" aria-label="smile">
+          &#128523;
+        </span>
+      </p>
     </footer>
   );
 }
